f1f359a61389a8cfd401e46c4a45b414184a9ba6
anil <anilbhomi201@gmail.com>
2018-05-30 20:46:03 +0545

    binding adapter

--- app/src/main/java/com/example/ebpearls/kotlinmvvm/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/ebpearls/kotlinmvvm/viewModel/LoginViewModel.kt
@@ -1,28 +1,48 @@
 package com.example.ebpearls.kotlinmvvm.viewModel
 
+import android.R
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.databinding.BindingAdapter
 import android.databinding.ObservableField
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.ebpearls.kotlinmvvm.model.LoginModel
 import com.example.ebpearls.kotlinmvvm.remote.LoginRepository
 
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel() : ViewModel() {
 
+    val quantityEntries=ObservableField<List<Int>>()
     var email = ObservableField("")
     var password = ObservableField("")
-
     var response: MutableLiveData<LoginModel>
 
     private val loginRepository: LoginRepository
 
     init {
+
         loginRepository = LoginRepository()
         response = loginRepository.getData(email.get()!!, password.get()!!)
     }
 
     fun login() {
+        val a:List<Int> = listOf(1,2,3,4,5,6)
+        quantityEntries.set(a)
         response = loginRepository.getData(email.get()!!, password.get()!!)
     }
+
+    companion object {
+        @BindingAdapter("entries")
+        @JvmStatic
+        fun setEntries(spinner: Spinner,entries: List<Int>?) {
+            if (entries != null) {
+                val arrayAdapter = ArrayAdapter(spinner.context, R.layout.simple_spinner_item, entries)
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = arrayAdapter
+            }
+        }
+    }
+
 }
